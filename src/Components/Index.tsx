8bfc3e6c4249e5af4ef01fda20316fc5b096b1a6
Pranav <prnvswrp@gmail.com>
2025-08-15 00:16:51 -0400

    Setup code coverage and necessary excludes

--- src/Components/Index.tsx
+++ src/Components/Index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Styles/Index.css'
-import App from './App.tsx'
+import '../Styles/Landing.css'
+import Landing from './Landing.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Landing />
   </StrictMode>,
 )
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,13 +6,22 @@ export default defineConfig({
         environment: 'jsdom',
         setupFiles: './src/Tests/setup',
         coverage: {
-            provider: 'v8',
-            thresholds: {                  // optional: fail build if below
+            provider: 'v8',         // or 'istanbul'
+            thresholds: {           // optional: fail build if below
                 statements: 80,
                 branches: 70,
                 functions: 80,
                 lines: 80,
             },
+            exclude: [
+                // Defaults
+                '**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**',
+                '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+                './src/vite-env.d.ts',
+                // Components
+                './src/Components/Index.tsx',
+                './src/Components/App.tsx'
+            ]
         }
     }
 })
